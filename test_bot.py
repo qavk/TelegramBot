1a6a5b203644689ad57cf260d12abb5f02becdc6
qavk <aan5@mail.ru>
2019-10-10 15:32:11 +0300

    first commit

---
+++ test_bot.py
@@ -0,0 +1,21 @@
+import telebot
+from telebot import apihelper
+
+apihelper.proxy = {'https': 'https://192.53.40.221:8080'}
+
+
+@bot.message_handler(content_types=['text'])
+def get_messages(message):
+
+    output_message = "########################################\n" \
+                     "Информация о человеке:\n" \
+                     "Имя: " + str(message.from_user.first_name) + "; " + "Никнейм: " + \
+                     str(message.from_user.username) + "; " + "ID: " + str(message.from_user.id) + "\n" \
+                     "Информация о чате:\n" \
+                     "Имя чата: " + str(message.chat.title) + "; " "ID чата: " + str(message.chat.id) + "\n" \
+                     "Сообщение: " + str(message.text) + "\n" \
+                     "########################################\n"
+    bot.send_message(my_id, output_message)
+
+
+bot.polling(none_stop=True)
